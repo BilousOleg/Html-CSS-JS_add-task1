d4745cb51cba3f2cc5fa5b76289ece6006f827ae
Bilous Oleg <olezhka82802@gmail.com>
2026-01-22 02:48:49 +0200

    Add third-exercise solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,17 @@
 function createLimiter(limit) {
-  return function () {};
+  let callsCount = 0;
+  return function () {
+    if (callsCount >= limit) {
+      // Оператор throw для генерації та викидання "кастомної" помилки
+      throw new Error('Exceeded function call limit');
+    }
+    // Що робить функція не вказано, тому для наглядності вивід в консоль
+    console.log('Hello');
+    callsCount++;
+  };
 }
+
+const limited = createLimiter(2);
+limited();
+limited();
+limited();
