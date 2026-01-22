adf992aa4fb0a5defbf65ec6ccefda90c51627be
Bilous Oleg <olezhka82802@gmail.com>
2026-01-22 18:11:28 +0200

    Add more code readability using arrow functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
-function createLimiter(limit) {
+/**
+ * Function gets limit of calls, create counter and returns function that can't be called more than [limit] times using closure
+ * @param {number} limit limit of function calls
+ * @returns
+ */
+const createLimiter = (limit) => {
   let callsCount = 0;
-  return function () {
+  return () => {
     if (callsCount >= limit) {
       // Оператор throw для генерації та викидання "кастомної" помилки
       throw new Error('Exceeded function call limit');
@@ -9,9 +14,12 @@ function createLimiter(limit) {
     console.log('Hello');
     callsCount++;
   };
-}
+};
 
 const limited = createLimiter(2);
 limited();
 limited();
 limited();
+
+// В цьому завданні стрілочна функція не дає такого сильного ефекту, як у попередніх,
+// тому що окрім повернення (return) виконується ще декілька дій, доводиться все обгортати в фігурні дужки
